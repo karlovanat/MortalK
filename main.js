e737ab6a1fef22394162f3ec4ca53c9aa7c244d9
karlovanat <suvf8@mail.ru>
2021-06-06 09:06:13 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
   const $arenas = document.querySelector('.arenas');
 
 const player1 = {
+    player: 1,
     name: 'Scorpion',
     hp: 100,
     img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
@@ -11,12 +12,13 @@ const player1 = {
     }
     
     const player2 = {
+        player: 2,
         name: 'Subzero',
         hp: 100,
         img: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
         weapon: ['ice'],
         attack: function (){
-            console.log(player2.name + ' ' +  'Fight!')
+            console.log(player2.name + ' ' + 'Fight!')
         },
     }
     
@@ -33,14 +35,21 @@ const player1 = {
         return $tag
     }
 
-    function createPlayer (player, playerX){
+    function createPlayer (playerX){
 
-    const $player = createElement('div', 'player');
+    const $player = createElement('div', 'player'+ playerX.player);
     const $progressbar = createElement('div', 'progressbar');
     const $life = createElement('div', 'life');
     const $name = createElement('div', 'name');
     const $character = createElement('div', 'character');
     const $img = createElement('img');
+    
+    
+    $player.classList.add(player);
+    $progressbar.classList.add('progressbar');
+    $name.classList.add('name');
+    $life.classList.add('life');
+    $character.classList.add('character');
     
     $img.src = playerX.img;
     $name.innerText = playerX.name;
@@ -57,7 +66,7 @@ const player1 = {
    return $player;
 }
 
-$arenas.appendChild(createPlayer('player1', player1));
-$arenas.appendChild(createPlayer('player2', player2));
+$arenas.appendChild(createPlayer(player1));
+$arenas.appendChild(createPlayer(player2));
 //    createPlayer('player1', player1);
 //    createPlayer('player2', player2);
